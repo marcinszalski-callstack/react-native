9c93d888c805180c9690f8f8624b346149d77b5e
Kamil Paradowski <kamil.paradowski@callstack.com>
2025-12-29 14:37:54 +0100

    feat(android): cleaner clip path application in drawing methods

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.kt
@@ -1234,16 +1234,8 @@ public open class ReactEditText public constructor(context: Context) : AppCompat
   }
 
   public override fun draw(canvas: Canvas) {
-    val clipPath = BackgroundStyleApplicator.getClipPath(this)
-    if (clipPath != null) {
-      canvas.save()
-      BackgroundStyleApplicator.applyClipPathIfPresent(this, canvas)
-    }
-
-    super.draw(canvas)
-
-    if (clipPath != null) {
-      canvas.restore()
+    BackgroundStyleApplicator.applyClipPathIfPresent(this, canvas) {
+      super.draw(canvas)
     }
   }
 
